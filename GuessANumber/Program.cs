422f9a3a4f0d87a455c5febddef70561cfcc33b5
Bozhidara <bozhidaraz@abv.bg>
2024-01-23 20:04:41 +0200

    added game difficulty

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -4,36 +4,46 @@
     {
         static void Main(string[] args)
         {
+            int levelup = 101;
             while (true)
             {
                 Random random = new Random();
-                int number = random.Next(1, 101);
+                int number = random.Next(1, levelup);
 
                 string input = "";
                 int guessNumber = 0;
 
+                int tries = 0;
                 while (true)
                 {
+                    if (tries >= 6)
+                    {
+                        Console.WriteLine("You didn't guess the number. Try again!");
+                        break;
+                    }
                     guessNumber = 0;
                     Console.WriteLine("Enter a number to guess: ");
                     input = Console.ReadLine();
                     bool isNumber = int.TryParse(input, out guessNumber);
-                    if (!isNumber || guessNumber < 1 || guessNumber > 100)
+                    if (!isNumber || guessNumber < 1 || guessNumber > (levelup - 1))
                     {
-                        Console.WriteLine("Please enter a valid number in range of 1 - 100");
+                        Console.WriteLine($"Please enter a valid number in range of 1 - {levelup - 1}");
                         continue;
                     }
                     if (guessNumber > number)
                     {
                         Console.WriteLine("Too high!");
+                        tries++;
                     }
                     else if (guessNumber < number)
                     {
                         Console.WriteLine("Too low!");
+                        tries++;
                     }
                     else
                     {
                         Console.WriteLine("You guessed the number");
+                        levelup += 100;
                         break;
                     }
                 }
